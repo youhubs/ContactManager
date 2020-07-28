2908f217cb3936dc3944308a1c9d85cad232ccc2
Hu Tiger <huxy99@yahoo.com>
2020-07-27 23:56:18 -0500

    config Spring MVC

---
+++ src/com/youhubs/contact/config/SpringMvcConfig.java
@@ -0,0 +1,46 @@
+package com.youhubs.contact.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.youhubs.contact.dao.ContactDAO;
+import com.youhubs.contact.dao.ContactDAOImpl;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.youhubs.contact")
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("Tigerhu123");
+
+		return dataSource;
+	}
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+
+	@Bean
+	public ContactDAO getContactDAO() {
+		return new ContactDAOImpl(getDataSource());
+	}
+}
